7a728e371b8146fb74714863f0043a3debd67f70
ahzs645 <ahzs645@gmail.com>
2025-10-13 15:56:37 -0700

    Remove legacy components and assets related to the PreGame and Desktop views, including Login, Splash, UserAccount, Taskbar, and Program components. Delete associated styles, tests, and context providers. Clean up unused audio files and images, streamlining the project structure for better maintainability.

--- src/legacy/views/Introduction/styles.js
+++
@@ -1,6 +0,0 @@
-// Libraries
-import styled from 'styled-components';
-// Components | Utils
-import { styledComponents } from '../../globalStyles';
-
-export const IntroductionScreen = styled(styledComponents.Screen)``;
