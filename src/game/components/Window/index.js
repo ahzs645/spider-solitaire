aaa5bf796ab93d108564d4d162075f8aa9ebaa7c
ahzs645 <ahzs645@gmail.com>
2025-12-02 16:10:05 -0800

    Refactor code structure for improved readability and maintainability

--- src/game/components/Window/index.js
+++ src/game/components/Window/index.js
@@ -6,8 +6,24 @@ import StatusBar from '../StatusBar';
 // Assets
 import * as Styled from './styles';
 
+// Check if running in embedded mode (inside iframe or with ?embedded=true)
+const isEmbedded = () => {
+  try {
+    // Check URL parameter
+    const params = new URLSearchParams(window.location.search);
+    if (params.get('embedded') === 'true') return true;
+    // Check if in iframe
+    if (window.self !== window.top) return true;
+  } catch (e) {
+    // If we can't access window.top due to cross-origin, we're in an iframe
+    return true;
+  }
+  return false;
+};
+
 const Window = (props) => {
   const { children } = props;
+  const embedded = isEmbedded();
 
   /*
   ====================================================
@@ -17,9 +33,9 @@ const Window = (props) => {
 
   return (
     <Styled.Window>
-      <WindowMenu />
+      {!embedded && <WindowMenu />}
       <Styled.WindowBody>{children}</Styled.WindowBody>
-      <StatusBar />
+      {!embedded && <StatusBar />}
     </Styled.Window>
   );
 };
